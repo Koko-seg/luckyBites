309eb5f4924b3c202e6dd3d71ae45433326d28dc
Tsetsko <tseko0301@gmail.com>
2025-09-02 12:27:28 +0800

    use client 2 page dr bichsen

--- src/app/(lobby)/games/runnerGame/page.tsx
+++ src/app/(lobby)/games/runnerGame/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import RunnerGame from "./components/RunnerGame";
 
 export default function RunnerGamePage() {
-  return <RunnerGame />;
+  return (
+    <Suspense>
+      <RunnerGame />
+    </Suspense>
+  );
 }
